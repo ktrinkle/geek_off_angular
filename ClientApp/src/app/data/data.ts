6e7701b014fc1c1bff7cbc8f6dae03e69bbf3e37
Kristin <kristin.russell@aa.com>
2021-09-22 20:27:34 -0400

    second

--- ClientApp/src/app/data/data.ts
+++ ClientApp/src/app/data/data.ts
@@ -1,26 +1,22 @@
-export interface round2SurveyQuestions
-{
+export interface round2SurveyQuestions {
   questionNo: number,
   surveyAnswers?: round2Answers[]
 }
 
-export interface round2Answers
-{
+export interface round2Answers {
   questionNo: number,
   answer: string,
   score: number
 }
 
-export interface round2Display
-{
+export interface round2Display {
   teamNo: number,
   player1Answers: round2Answers[],
   player2Answers?: round2Answers[],
   finalScore: number
 }
 
-export interface round23Scores
-{
+export interface round23Scores {
   teamNo: number,
   teamName: string,
   teamScore?: number,
--- ClientApp/src/app/store/round2/round2.reducer.ts
+++ ClientApp/src/app/store/round2/round2.reducer.ts
@@ -1,14 +1,16 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { round2SurveyQuestions } from 'src/app/data/data';
 
 
 export const round2FeatureKey = 'round2';
 
 export interface State {
+  surverylist: Array<Round2SurveyList>,
 
 }
 
 export const initialState: State = {
-
+  SurveryList = new Array < round2SurveyQuestions
 };
 
 
