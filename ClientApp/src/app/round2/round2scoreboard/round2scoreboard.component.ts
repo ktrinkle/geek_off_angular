5ba6ef9c04d28742587a1fcc7aba3dce8caae34e
granthill7 <51276876+granthill7@users.noreply.github.com>
2021-10-11 16:12:45 -0500

    Round2scoreboard (#42)
    * Formatted grid
    
    * Finished control functionality
    
    * Fixed merge conflicts
    
    * Round 2 Scoreboard
    
    * added font to readme

---
+++ ClientApp/src/app/round2/round2scoreboard/round2scoreboard.component.ts
@@ -0,0 +1,42 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { round23Scores } from 'src/app/data/data';
+import { DataService } from '../../data.service';
+
+@Component({
+  selector: 'app-round2-scoreboard',
+  templateUrl: './round2scoreboard.component.html',
+  styleUrls: ['./round2scoreboard.component.scss']
+})
+export class Round2scoreboardComponent implements OnInit {
+
+  constructor(private _dataService: DataService) { }
+
+  public yevent: string = 'e21';
+  public roundNo: number = 2;
+  public scores: round23Scores[] = [];
+  public colors: string[] = [
+    'coral',
+    'steelblue',
+    'gold',
+    'lightgreen',
+    'sandybrown',
+    'plum',
+  ]
+
+  ngOnInit(): void {
+    this.getScoreboardInfo(this.yevent);
+    console.log("End of Init");
+  }
+
+  public async getScoreboardInfo(yevent: string) {
+    await this._dataService.getRound2Scores(yevent).subscribe((data: round23Scores[]) => {
+      this.scores = data.sort((a, b) => a.teamScore? - (b.teamScore || 0) : 0);
+
+      this.scores.forEach((score, index) => {
+        score.color = this.colors[index];
+      });
+
+      console.log(this.scores);
+    });
+  }
+}
