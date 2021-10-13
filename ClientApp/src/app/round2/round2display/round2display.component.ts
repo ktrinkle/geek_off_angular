9d1b4dd30b502cfaf0e3bdae9654e182d93a4ff1
Dan Mullings <damcis@gmail.com>
2021-10-13 18:58:44 -0500

    Added animation

--- ClientApp/src/app/round2/round2display/round2display.component.ts
+++ ClientApp/src/app/round2/round2display/round2display.component.ts
@@ -7,20 +7,48 @@ import { DataService } from 'src/app/data.service';
 import { round2Answers, round2Display, round2SurveyList } from 'src/app/data/data';
 import { selectRound2AllSurvey } from 'src/app/store';
 import { round2AllSurvey } from 'src/app/store/round2/round2.actions';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import * as signalR from '@microsoft/signalr';
+import { environment } from 'src/environments/environment';
 
 export interface displayRow {
   player1: round2Answers;
   player2: round2Answers;
 }
+
 @Component({
   selector: 'app-round2display',
   templateUrl: './round2display.component.html',
-  styleUrls: ['./round2display.component.scss']
+  styleUrls: ['./round2display.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate(10000)),
+    ]),
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179.9deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('5000ms ease-out')),
+      // transition('inactive => active', animate('500ms ease-in'))
+      transition('inactive => active', animate('5000ms ease-in'))
+    ])
+  ]
 })
+
 export class Round2displayComponent implements OnInit, OnDestroy {
   @ViewChild(MatTable, { static: true }) table!: MatTable<any>;
   destroy$: Subject<boolean> = new Subject<boolean>();
-  yEvent = 'e21'
+  yEvent = 'e21';
+  displayStatus = 0;
   teamNumber = 1;
   displayObject: round2Display = {
     teamNo: 0,
