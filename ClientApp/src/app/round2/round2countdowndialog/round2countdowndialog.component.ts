e153ecb657950c0e91407a57e3a2145f6ba06ee6
Diyalo Manral <diyalo.manral@aa.com>
2021-10-16 15:38:15 -0700

    Final countdown fixes.

---
+++ ClientApp/src/app/round2/round2countdowndialog/round2countdowndialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  countdownValue: number;
+}
+
+@Component({
+  selector: 'app-round2countdowndialog',
+  templateUrl: './round2countdowndialog.component.html',
+  styleUrls: ['./round2countdowndialog.component.scss']
+})
+export class Round2countdowndialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<Round2countdowndialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  ngOnInit(): void {
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
