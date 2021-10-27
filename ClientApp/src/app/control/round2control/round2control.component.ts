6e9c7bf33c696c51cdf1f0148a6eedb1ba4cfe28
Kristin <kristin.russell@aa.com>
2021-10-27 10:50:38 -0700

    something

--- ClientApp/src/app/control/round2control/round2control.component.ts
+++ ClientApp/src/app/control/round2control/round2control.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
 import { Store } from '@ngrx/store';
 import { round2AllSurvey } from '../../store/round2/round2.actions';
 import { round2SurveyQuestions, round2SubmitAnswer, round2Answers, round23Scores } from '../../data/data';
-import { FormGroup, FormControl, Validators, FormBuilder, Form } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 import * as signalR from '@microsoft/signalr';
 import { environment } from 'src/environments/environment';
-
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { Round2countdowndialogComponent } from 'src/app/round2/round2countdowndialog/round2countdowndialog.component';
+
 @Component({
   selector: 'app-round2control',
   templateUrl: './round2control.component.html',
   styleUrls: ['./round2control.component.scss']
 })
 export class Round2controlComponent implements OnInit {
-
   public yevent = sessionStorage.getItem('event') ?? '';
   public surveyMasterList: round2SurveyQuestions[] = [];
-  public newEventForm: FormGroup = new FormGroup({});
-  public questionNum: number = 0;
-  public teamNum: number = 0;
-  public playerNum: number = 0;
-  public answer: string = '';
-  public score: number = 0;
+  public newEventForm: FormGroup = new FormGroup({
+    teamNum: new FormControl('', [Validators.pattern('[0-9]*')]),
+    playerNum: new FormControl('', [Validators.pattern('1|2')]),
+    questions: new FormArray([]),
+  });
   public apiResponse: string = '';
   public showBonusQuestion: boolean = false;
   public scoreboard: round23Scores[] = [];
@@ -40,27 +38,27 @@ export class Round2controlComponent implements OnInit {
   })
 
   displayList: any[] = [
-    {key: 0, value: 'Hide all'},
-    {key: 1, value: 'Player 1 Answer 1'},
-    {key: 2, value: 'Player 1 Score 1'},
-    {key: 3, value: 'Player 1 Answer 2'},
-    {key: 4, value: 'Player 1 Score 2'},
-    {key: 5, value: 'Player 1 Answer 3'},
-    {key: 6, value: 'Player 1 Score 3'},
-    {key: 7, value: 'Player 1 Answer 4'},
-    {key: 8, value: 'Player 1 Score 4'},
-    {key: 9, value: 'Player 1 Answer 5'},
-    {key: 10, value: 'Player 1 Score 5'},
-    {key: 11, value: 'Player 2 Answer 1'},
-    {key: 12, value: 'Player 2 Score 1'},
-    {key: 13, value: 'Player 2 Answer 2'},
-    {key: 14, value: 'Player 2 Score 2'},
-    {key: 15, value: 'Player 2 Answer 3'},
-    {key: 16, value: 'Player 2 Score 3'},
-    {key: 17, value: 'Player 2 Answer 4'},
-    {key: 18, value: 'Player 2 Score 4'},
-    {key: 19, value: 'Player 2 Answer 5'},
-    {key: 20, value: 'Player 2 Score 5'},
+    { key: 0, value: 'Hide all' },
+    { key: 1, value: 'Player 1 Answer 1' },
+    { key: 2, value: 'Player 1 Score 1' },
+    { key: 3, value: 'Player 1 Answer 2' },
+    { key: 4, value: 'Player 1 Score 2' },
+    { key: 5, value: 'Player 1 Answer 3' },
+    { key: 6, value: 'Player 1 Score 3' },
+    { key: 7, value: 'Player 1 Answer 4' },
+    { key: 8, value: 'Player 1 Score 4' },
+    { key: 9, value: 'Player 1 Answer 5' },
+    { key: 10, value: 'Player 1 Score 5' },
+    { key: 11, value: 'Player 2 Answer 1' },
+    { key: 12, value: 'Player 2 Score 1' },
+    { key: 13, value: 'Player 2 Answer 2' },
+    { key: 14, value: 'Player 2 Score 2' },
+    { key: 15, value: 'Player 2 Answer 3' },
+    { key: 16, value: 'Player 2 Score 3' },
+    { key: 17, value: 'Player 2 Answer 4' },
+    { key: 18, value: 'Player 2 Score 4' },
+    { key: 19, value: 'Player 2 Answer 5' },
+    { key: 20, value: 'Player 2 Score 5' },
   ]
 
   constructor(private store: Store, private _dataService: DataService,
@@ -93,50 +91,71 @@ export class Round2controlComponent implements OnInit {
     this.getSurveyQuestions(this.yevent);
     this.updateScoreboard();
 
-    this.newEventForm = new FormGroup({
-      teamNum: new FormControl('', [Validators.pattern('[0-9]*')]),
-      playerNum: new FormControl('', [Validators.pattern('1|2')]),
-      answer: new FormControl(''),
-      score: new FormControl('', [Validators.pattern('[0-9]*')])
-    });
   }
 
   // Grabs all the questions.
   getSurveyQuestions(yevent: string) {
     this._dataService.getAllRound2SurveyQuestions(yevent).subscribe((data: round2SurveyQuestions[]) => {
       this.surveyMasterList = data;
-      console.log(this.surveyMasterList);
+
+      // creates the formArray of the formGroup of question controls
+      const qArray = this.formBuilder.array([]);
+      for (let question of this.surveyMasterList) {
+        qArray.push(
+          this.formBuilder.group({
+            questionNum: new FormControl(question.questionNum),
+            answer: new FormControl(''),
+            score: new FormControl('', [Validators.pattern('[0-9]*')])
+          })
+        );
+      }
+
+      // creates the entire form
+      this.newEventForm = new FormGroup({
+        teamNum: new FormControl('', [Validators.pattern('[0-9]*')]),
+        playerNum: new FormControl('', [Validators.pattern('1|2')]),
+        questions: qArray,
+      });
     });
   }
 
-  // Get user answer and store in DB (see)
-  saveUserAnswer(question: any) {
-    console.log(this.newEventForm);
+  getQuestions(form: any) {
+    return form?.get('questions').controls;
+  }
 
+  getAnswers(questionNumber: any) {
+    const question = this.surveyMasterList.filter(x => x.questionNum == questionNumber);
+    if (question.length > 0) {
+      return question[0].surveyAnswers;
+    }
+    return [];
+  }
+
+  // Get user answer and store in DB (see)
+  saveUserAnswer(questionGroup: any) {
     var submitAnswer: round2SubmitAnswer = {
       yEvent: this.yevent,
-      questionNum: question.questionNum,
-      playerNum: this.newEventForm.value.playerNum,
-      teamNum: this.newEventForm.value.teamNum,
-      answer: this.newEventForm.value.answer,
-      score: this.newEventForm.value.score
+      questionNum: questionGroup.get('questionNum').value,
+      playerNum: this.newEventForm.get('playerNum')?.value,
+      teamNum: this.newEventForm.get('teamNum')?.value,
+      answer: questionGroup.get('answer').value,
+      score: questionGroup.get('score').value
     };
 
     console.log(submitAnswer);
 
     this._dataService.sendRound2AnswerText(submitAnswer).subscribe((data: string) => {
-                                              this.apiResponse = data;
+      this.apiResponse = data;
     });
 
-    this.newEventForm.get("answer")?.reset();
-    this.newEventForm.get("score")?.reset();
-    this.newEventForm.get("questionNum")?.reset();
-
+    questionGroup.get('answer').reset();
+    questionGroup.get('score').reset();
+    // this.newEventForm.get("questionNum")?.reset();
   }
 
   toggleBonusQuestion() {
     this.showBonusQuestion = !this.showBonusQuestion;
-    if(this.showBonusQuestion) {
+    if (this.showBonusQuestion) {
       console.log("Showing bonus...")
     }
     else {
@@ -144,44 +163,43 @@ export class Round2controlComponent implements OnInit {
     }
   }
 
-  presetAnswer(answer: round2Answers) {
-    this.newEventForm.get("answer")?.setValue(answer.answer);
-    this.newEventForm.get("score")?.setValue(answer.score);
-    this.newEventForm.get("questionNum")?.setValue(answer.questionNum);
+  presetAnswer(answer: any, question: FormGroup) {
+    question.get("answer")?.setValue(answer.answer);
+    question.get("score")?.setValue(answer.score);
+    question.get("questionNum")?.setValue(answer.questionNum);
   }
 
-  async updateRemoteScoreboard()
-  {
+  async updateRemoteScoreboard() {
     await this._dataService.updateScoreboardRound2();
     this.updateScoreboard();
   }
 
-  updateScoreboard()
-  {
-    this._dataService.getRound2Scores(this.yevent).subscribe({next: (a => {
-      this.scoreboard = a;
-    }), complete: () => {
-      this.scoreboard.sort((a,b) => 0 - ((a.teamScore ?? 0) > (b.teamScore ?? 0) ? 1 : -1));
-    }
-  });
+  updateScoreboard() {
+    this._dataService.getRound2Scores(this.yevent).subscribe({
+      next: (a => {
+        this.scoreboard = a;
+      }), complete: () => {
+        this.scoreboard.sort((a, b) => 0 - ((a.teamScore ?? 0) > (b.teamScore ?? 0) ? 1 : -1));
+      }
+    });
   }
 
   // may need to move this to howler
   playBuzzer() {
-    this.buzzer.play().then( _b => {
+    this.buzzer.play().then(_b => {
       this.buzzer.currentTime = 0;
       this.buzzer.pause;
     });
   }
 
   playDings() {
-    this.dings.play().then( _b => {
+    this.dings.play().then(_b => {
       this.dings.currentTime = 0;
       this.dings.pause;
     });
   }
 
-  showDisplayBoardValue(){
+  showDisplayBoardValue() {
     // this will be values 0-20. 0 = hide all, 10 = show all player 1, 20 = show all
     var entryNum = this.pickAnimateForm.value.currentDisplayId;
     this._dataService.revealRound2Value(entryNum);
@@ -190,14 +208,14 @@ export class Round2controlComponent implements OnInit {
   showFirstAnswers(form: FormGroup) {
     console.log("Team Num: " + form.get("teamNum")?.value);
     this._dataService.getRound2FirstPlayer(this.yevent, form.get("teamNum")?.value)
-                                               .subscribe((data: round2Answers[]) => this.firstPlayerAnswers = data);
+      .subscribe((data: round2Answers[]) => this.firstPlayerAnswers = data);
     console.log("FPA: " + this.firstPlayerAnswers);
   }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(Round2countdowndialogComponent, {
       width: '250px',
-      data: {countdownValue: this.countdownValue}
+      data: { countdownValue: this.countdownValue }
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -207,22 +225,21 @@ export class Round2controlComponent implements OnInit {
     });
   }
 
-  startCountdown(){
+  startCountdown() {
     this._dataService.startCountdown(this.countdownValue);
   }
 
-  stopCountdown(){
+  stopCountdown() {
     this._dataService.stopCountdown();
   }
 
-  setCountdown(seconds: number){
+  setCountdown(seconds: number) {
     this._dataService.setCountdown(Number(seconds));
   }
 
-  changeScreen(name: string){
+  changeScreen(name: string) {
     this._dataService.changeRound2Page(name);
-    if(name == 'prize1')
-    {
+    if (name == 'prize1') {
       this.consolation.play();
     }
   }
