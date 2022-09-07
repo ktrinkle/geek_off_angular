b2621e32f4f233832c831c49ae442649c2641126
Kevin Trinkle <trinkle@psa-history.org>
2022-09-07 07:08:24 -0700

    Tweaking role handler

--- API/Extensions/AuthorizeAttribute.cs
+++ API/Extensions/AuthorizeAttribute.cs
@@ -21,17 +21,52 @@ public class AuthorizeAttribute : Attribute, IAuthorizationFilter
     public void OnAuthorization(AuthorizationFilterContext context)
     {
         // skip authorization if action is decorated with [AllowAnonymous] attribute
+        var notAuth = false;
         var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
         if (allowAnonymous)
         {
             return;
         }
 
+        // a very poor implementation of early exit
         var teamNum = context.HttpContext.Items["User"];
+        if (teamNum is null)
+        {
+            notAuth = true;
+        }
+        var teamCheck = int.TryParse(teamNum.ToString(), out var teamNumInt);
+        if (!teamCheck || teamNumInt < 0)
+        {
+            notAuth = true;
+        }
+
         var adminName = context.HttpContext.Items["Name"];
-        Enum.TryParse<Role>(context.HttpContext.Items["Role"].ToString(), out Role roleName);
-        // Doesn't always work
-        if (teamNum == null && adminName == null && (!_roles.Any() && !_roles.Contains(roleName)))
+        if (teamNumInt > 0 && adminName is null)
+        {
+            notAuth = false;
+        }
+
+        if (teamNumInt == 0 && adminName is null)
+        {
+            notAuth = true;
+        }
+
+        var roleTest = Enum.TryParse<Role>(context.HttpContext.Items["Role"].ToString(), out var roleName);
+        if (roleTest == false)
+        {
+            notAuth = true;
+        }
+
+        if (_roles.Any() && _roles.Contains(roleName) && notAuth != true)
+        {
+            notAuth = false;
+        }
+        else
+        {
+            notAuth = true;
+        }
+
+        if (notAuth)
         {
             // not logged in
             context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
